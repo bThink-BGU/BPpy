609eec9ca99a2ae90b616e7fb3ff5886045620b9
geraw <gera.weiss@gmail.com>
2019-05-07 14:21:31 +0300

    Merge with Gera's code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from execution.listeners.print_b_program_runner_listener import PrintBProgramRunnerListener
 from model.bprogram import BProgram
-from model.event_selection.smt_event_selection_strategy import SMTEventSelectionStrategy
+from model.event_selection.simple_event_selection_strategy import SimpleEventSelectionStrategy
 
 if __name__ == "__main__":
-    # b_program = Bprogram(source_name="hot_cold_bath",
-    #                      event_selection_strategy=SimpleEventSelectionStrategy(),
-    #                      listener=PrintBProgramRunnerListener())
-    b_program = BProgram(source_name="hot_cold_smt",
-                         event_selection_strategy=SMTEventSelectionStrategy(),
+    b_program = BProgram(source_name="hot_cold_bath",
+                         event_selection_strategy=SimpleEventSelectionStrategy(),
                          listener=PrintBProgramRunnerListener())
+    # b_program = BProgram(source_name="hot_cold_smt",
+    #                      event_selection_strategy=SMTEventSelectionStrategy(),
+    #                      listener=PrintBProgramRunnerListener())
     b_program.run()
