60756cd14c337da6e09f7c1aec45617e861300cc
tomyaacov <tomyaacov1210@gmail.com>
2019-07-21 16:02:05 +0300

    examples refactoring

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,4 +1,7 @@
 from model.b_event import BEvent
+from execution.listeners.print_b_program_runner_listener import PrintBProgramRunnerListener
+from model.bprogram import BProgram
+from model.event_selection.simple_event_selection_strategy import SimpleEventSelectionStrategy
 
 
 def hello():
@@ -9,8 +12,8 @@ def world():
     yield {'request': BEvent(name="World")}
 
 
-"""request, waitFor, block, interrupt"""
-
-
-
-
+if __name__ == "__main__":
+    b_program = BProgram(bthreads=[hello(), world()],
+                         event_selection_strategy=SimpleEventSelectionStrategy(),
+                         listener=PrintBProgramRunnerListener())
+    b_program.run()
--- examples/hot_cold_bath.py
+++ examples/hot_cold_bath.py
@@ -1,4 +1,8 @@
 from model.b_event import BEvent
+from execution.listeners.print_b_program_runner_listener import PrintBProgramRunnerListener
+from model.bprogram import BProgram
+from model.event_selection.simple_event_selection_strategy import SimpleEventSelectionStrategy
+
 
 
 def add_hot():
@@ -17,3 +21,11 @@ def control_temp():
     while True:
         yield {'waitFor': BEvent(name="COLD"), 'block': BEvent(name="HOT")}
         yield {'waitFor': BEvent(name="HOT"), 'block': BEvent(name="COLD")}
+
+
+if __name__ == "__main__":
+    b_program = BProgram(bthreads=[add_hot(), add_cold(), control_temp()],
+                         event_selection_strategy=SimpleEventSelectionStrategy(),
+                         listener=PrintBProgramRunnerListener())
+    b_program.run()
+
