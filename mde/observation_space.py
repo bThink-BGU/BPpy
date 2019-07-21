b6b560d124e67346ab7deeebd5528d5c70b64599
tomyaacov <tomyaacov1210@gmail.com>
2019-07-21 15:49:04 +0300

    mde updates

--- mde/observation_space.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from gym import spaces
-
-low = np.array([-100, -48.25, -28.75, -48.25, -28.75, 0])
-high = np.array([100, 48.25, 28.75, 48.25, 28.75, 360])
-
-observation_space = spaces.Box(low, high, dtype=np.float32)
