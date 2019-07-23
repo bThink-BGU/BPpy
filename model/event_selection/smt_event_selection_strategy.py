63b6e51306b118626bccd742e67fc11421781ed0
tomyaacov <tomyaacov1210@gmail.com>
2019-07-23 10:26:40 +0300

    smt event selection - using z3helper

--- model/event_selection/smt_event_selection_strategy.py
+++ model/event_selection/smt_event_selection_strategy.py
@@ -1,27 +1,23 @@
 from model.event_selection.event_selection_strategy import EventSelectionStrategy
-from z3 import *
+from utils.z3helper import *
 
 
 class SMTEventSelectionStrategy(EventSelectionStrategy):
 
     def is_satisfied(self, event, statement):
-        return is_true(event.eval(statement.get('wait-for', self.true)))  # TODO: not sure if it's the right approach
-
-    def __init__(self):
-        self.true = BoolSort().cast(True)
-        self.false = BoolSort().cast(False)
+        return is_true(event.eval(statement.get('wait-for', true)))  # TODO: not sure if it's the right approach
 
     def select(self, statements, additional_statement=None):
-        (request, block) = (self.false, self.false)  # TODO: need to change request back to false
+        (request, block) = (false, false)
 
         # Collect request and block statements
         for l in statements:
-            request = Or(request, l.get('request', self.false))
-            block = Or(block, l.get('block', self.false))
+            request = Or(request, l.get('request', false))
+            block = Or(block, l.get('block', false))
 
         if additional_statement:
-            request = Or(request, additional_statement.get('request', self.false))
-            block = Or(block, additional_statement.get('block', self.false))
+            request = Or(request, additional_statement.get('request', false))
+            block = Or(block, additional_statement.get('block', false))
 
 
         # Compute a satisfying assignment
