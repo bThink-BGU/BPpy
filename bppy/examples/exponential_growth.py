fc7a2e270ba4f73270c16d6a563bd9df93c6b60a
Meir Maor Biton <62711162+maor226@users.noreply.github.com>
2022-11-18 19:13:58 +0200

    Add files via upload

---
+++ bppy/examples/exponential_growth.py
@@ -0,0 +1,17 @@
+from bppy import *
+
+
+@b_thread
+def exponential_growth(exp):
+    yield {request: BEvent(f"{exp}")}
+    if exp > 0:
+        b_program.add_bthread(exponential_growth(exp - 1))
+        b_program.add_bthread(exponential_growth(exp - 1))
+
+
+if __name__ == "__main__":
+    global b_program
+    b_program = BProgram(bthreads=[exponential_growth(4)],
+                         event_selection_strategy=SimpleEventSelectionStrategy(),
+                         listener=PrintBProgramRunnerListener())
+    b_program.run()
