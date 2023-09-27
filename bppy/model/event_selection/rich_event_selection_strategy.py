2114dd12542c1f0eac482d186b71286a8f38aed6
tomyaacov <tomyaacov1210@gmail.com>
2023-09-27 18:07:12 +0300

    updating smt_event_selection_strategy and adding rich_event_selection_strategy

---
+++ bppy/model/event_selection/rich_event_selection_strategy.py
@@ -0,0 +1,14 @@
+from bppy.model.event_selection.smt_event_selection_strategy import SMTEventSelectionStrategy
+from bppy.utils.z3helper import *
+
+
+class RichEventSelectionStrategy(SMTEventSelectionStrategy):
+    """
+    A solver-based implementation of EventSelectionStrategy using z3 SMT solver. The strategy used is the one
+    presented in the paper `Executing Scenario-Based Specification with Dynamic Generation of Rich Events
+    <https://www.wisdom.weizmann.ac.il/~dharel/papers/CCIS2019RichEvents.pdf>`_.
+    """
+
+    def is_satisfied(self, event, statement):
+        return is_true(event.eval(statement.get('waitFor', true)))
+
