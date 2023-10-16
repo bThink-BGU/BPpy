d84c3b0e752e91f49b4d20fbb54ead15a0d140a2
tomyaacov <tomyaacov1210@gmail.com>
2023-10-16 10:21:03 +0300

    bp.sync class

--- bppy/model/b_thread.py
+++ bppy/model/b_thread.py
@@ -1,7 +1,8 @@
 from copy import copy
+from warnings import warn
 
 
-def b_thread(func):
+def thread(func):
     """
     A decorator to wrap bthread generator with, in order to handle data transmission and bthread termination.
     """
@@ -12,13 +13,23 @@ def b_thread(func):
             while True:
                 try:
                     e = f.send(m)
+                    if type(e) == dict:
+                        warn("using dict for statements is deprecated, use bppy.model.sync_statement.sync instead.")
                     if isinstance(e, dict):
                         e["locals"] = copy(f.gi_frame.f_locals)
-                    m = yield e
-                    if m is None:
-                        break
+                        m = yield e
+                        if m is None:
+                            break
+                    else:
+                        raise TypeError("bthread must yield a bppy.model.sync_statement.sync object")
                 except (KeyError, StopIteration):
                     m = yield None
                     break
     return wrapper
+
+
+def b_thread(func):
+    warn("the b_thread decorator is deprecated, use thread instead.")
+    return thread(func)
+
 
--- bppy/model/sync_statement.py
+++ bppy/model/sync_statement.py
@@ -3,5 +3,21 @@ waitFor = "waitFor"
 block = "block"
 mustFinish = "mustFinish"
 priority = "priority"
-localReward = "reward"
+localReward = "localReward"
 
+
+class sync(dict):
+    def __init__(self, *, request=None, waitFor=None, block=None, mustFinish=None, priority=None, localReward=None, **kwargs):
+        if request is not None:
+            self["request"] = request
+        if waitFor is not None:
+            self["waitFor"] = waitFor
+        if block is not None:
+            self["block"] = block
+        if mustFinish is not None:
+            self["mustFinish"] = mustFinish
+        if priority is not None:
+            self["priority"] = priority
+        if localReward is not None:
+            self["localReward"] = localReward
+        super().__init__(kwargs)
