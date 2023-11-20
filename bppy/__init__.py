f724f6bbebd56f864ac5f03f5466eea01210c62c
tomyaacov <tomyaacov1210@gmail.com>
2023-11-20 11:54:44 +0200

    bumping version

--- bppy/__init__.py
+++ bppy/__init__.py
@@ -1,7 +1,7 @@
 from os import listdir
 from os.path import dirname, basename, isfile, join, isdir
 
-__version__ = '0.0.11'
+__version__ = '1.0.0'
 # TODO: find a smarter way to import all submodules
 from bppy.execution.listeners.b_program_runner_listener import *
 from bppy.execution.listeners.print_b_program_runner_listener import *
